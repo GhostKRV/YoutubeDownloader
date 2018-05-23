f6e445c4c157b5da41d67f17882a405051cb8654
Andrey Samoylov <andrey95100@gmail.com>
2018-05-23 21:41:20 +0300

    add gitignnore and add enter url for download

--- src/main/java/downloader/YTD.java
+++ src/main/java/downloader/YTD.java
@@ -9,20 +9,17 @@ import java.util.*;
 public class YTD {
 
     public static void main(String[] args) {
-
-        Set<String> link = new LinkedHashSet<>();
-
-        String path = "F:\\Java\\Projects\\Maven\\# 0001 YoutubeDownloader\\downloads";
-        String url = "https://www.youtube.com/watch?v=bjh1HWV9RRk&list=PL786bPIlqEjRFPH8Z9IOwJWseG6Dq_Qxb&index=1";
-
-        HTMLParser htmp = new HTMLParser();
-        YoutubeDownloader ytd = new YoutubeDownloader(path);
-
         try {
+            Set<String> link = new LinkedHashSet<>();
+            BufferedReader inputURL = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter playlist URL: ");
+            String url = inputURL.readLine();
+            //String url = "https://www.youtube.com/watch?v=bjh1HWV9RRk&list=PL786bPIlqEjRFPH8Z9IOwJWseG6Dq_Qxb&index=1";
+            String path = "F:\\Java\\Projects\\Maven\\# 0001 YoutubeDownloader\\downloads";
+            HTMLParser htmp = new HTMLParser();
+            YoutubeDownloader ytd = new YoutubeDownloader(path);
             link = htmp.getLinks(url);
-
             System.out.println("Download from:" + url);
-
             for(String s: link){
                 if(s.contains("index=")) {
                     String downloadUrl = "https://www.youtube.com/" + s.substring(30, s.length() - 1);
@@ -30,7 +27,6 @@ public class YTD {
                 }
             }
         }
-
         catch (Exception e) {
             e.printStackTrace();
         }
